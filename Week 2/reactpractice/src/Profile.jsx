8641f1c25b21e994305e6fb4ef58185b216d1132
jyang-214 <jyang214@my.bcit.ca>
2023-04-18 20:30:15 -0700

    Update Profile.jsx

--- Week 2/reactpractice/src/Profile.jsx
+++ Week 2/reactpractice/src/Profile.jsx
@@ -1,7 +1,8 @@
 function Profile() {
+    const fname = "Jacky"
     return (
       <>
-        <h1>Your Profile</h1>
+        <h1>{fname}'s Profile</h1>
         <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
       </>
     );
