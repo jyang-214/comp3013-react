2e1e8e57454b137f4bd8666388a8e86af47b2750
jyang-214 <jyang214@my.bcit.ca>
2023-04-18 20:28:51 -0700

    Update App.jsx

--- Week 2/reactpractice/src/App.jsx
+++ Week 2/reactpractice/src/App.jsx
@@ -1,18 +1,10 @@
 import "./App.css";
+import Profile from "./Profile";
 
 // Components MUST START WITH A CAPITAL LETTER
 // PascalCasing: First letter in every word starts with a capital letter
 // return statements are either on one line
 // OR multiple lines with parentheses
-
-function Profile() {
-  return (
-    <>
-      <h1>Your Profile</h1>
-      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
-    </>
-  );
-}
 
 function App() {
   return (
