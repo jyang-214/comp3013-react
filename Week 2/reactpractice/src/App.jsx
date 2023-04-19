58b5e505cc59d36601fdb34f1a92a19728890957
jyang-214 <jyang214@my.bcit.ca>
2023-04-18 20:27:26 -0700

    Update

--- Week 2/reactpractice/src/App.jsx
+++ Week 2/reactpractice/src/App.jsx
@@ -1,13 +1,32 @@
+import "./App.css";
 
-import './App.css'
+// Components MUST START WITH A CAPITAL LETTER
+// PascalCasing: First letter in every word starts with a capital letter
+// return statements are either on one line
+// OR multiple lines with parentheses
+
+function Profile() {
+  return (
+    <>
+      <h1>Your Profile</h1>
+      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+    </>
+  );
+}
 
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <>
+      <h1>My Website</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+    </>
+  );
 }
 
-function Movies() {
-
-}
+function Movies() {}
 
 // export { App, Movies }  // Nmaed Export
-export default App   // when you have only one function that exports from this file
+export default App; // when you have only one function that exports from this file
---
+++ Week 2/reactpractice/src/Profile.jsx
@@ -0,0 +1,8 @@
+function Profile() {
+    return (
+      <>
+        <h1>Your Profile</h1>
+        <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+      </>
+    );
+  }
