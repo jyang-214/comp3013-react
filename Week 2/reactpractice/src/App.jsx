1f0d8e7a12866f33d0d4360c70eae7d2597ba5a2
jyang-214 <jyang214@my.bcit.ca>
2023-04-18 20:04:40 -0700

    Vite

--- Week 2/reactpractice/src/App.jsx
+++ Week 2/reactpractice/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello BCIT</h1>
+      <h1>Hello Week 2 BCIT</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
