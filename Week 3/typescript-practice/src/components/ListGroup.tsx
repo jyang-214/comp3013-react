5e0e6eca607dea2a4f8e2e9f93fbba2eb20f4d49
jyang-214 <jyang214@my.bcit.ca>
2023-04-25 21:00:57 -0700

    Week 3

--- Week 3/typescript-practice/src/components/ListGroup.tsx
+++ Week 3/typescript-practice/src/components/ListGroup.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ListItem from "./ListItem";
-// const cities: string[] = ["Vancouver", "London", "Paris", "Tokyo"];
-const cities: string[] = [];
 
 function ListGroup() {
-  // //Option 1
+  const cities: string[] = ["Vancouver", "London", "Paris"];
+  // const cities: string[] = [];
 
-  //   // Case 1: Empty Array
-  //   if (cities.length === 0) {
-  //     return <h3>No cities to select!</h3>;
-  //   }
-  //   // Case 2: Ciities in Array..Show Them
-  //   return (
-  //     <ul className="list-group">
-  //       {cities.map((c) => (
-  //         <ListItem city={c} />
-  //       ))}
-  //     </ul>
-  //   );
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
+  //Option 1
+  // Case 1: Empty Array
+  if (cities.length === 0) {
+    return <h3>No cities to select!</h3>;
+  }
+  // Case 2: Ciities in Array..Show Them
+  return (
+    <ul className="list-group">
+      {cities.map((c, index) => (
+        <ListItem key={c} city={c} index={index} />
+      ))}
+    </ul>
+  );
   // //Option 2
-
   // return (
   //   <>
   //     {/* Case 1: No cities in Array */}
   //     {cities.length === 0 ? <p>NO CITIES TO SELECT</p> : null}
-
   //     {/* Case 2: Cities in Array */}
   //     <ul className="list-group">
   //       {cities.map((c) => (
@@ -34,22 +33,22 @@ function ListGroup() {
   //     </ul>
   //   </>
   // );
-
-  // Option 3
-
-  return (
-    <>
-      {/* Case 1: No cities in Array */}
-      {cities.length === 0 && <p>NO CITIES TO SELECT</p>}
-
-      {/* Case 2: Cities in Array */}
-      <ul className="list-group">
-        {cities.map((c) => (
-          <ListItem city={c} />
-        ))}
-      </ul>
-    </>
-  );
+  //   // Option 3
+  // // true && true   // true
+  // // true && 1      // 1
+  // // false && 1     // false
+  //   return (
+  //     <>
+  //       {/* Case 1: No cities in Array */}
+  //       {cities.length === 0 && <p>NO CITIES TO SELECT</p>}
+  //       {/* Case 2: Cities in Array */}
+  //       <ul className="list-group">
+  //         {cities.map((c) => (
+  //           <ListItem city={c} />
+  //         ))}
+  //       </ul>
+  //     </>
+  //   );
 }
 
 export default ListGroup;
--- Week 3/typescript-practice/src/components/ListItem.tsx
+++ Week 3/typescript-practice/src/components/ListItem.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   city: string;
+  index: number;
 }
 
-function ListItem({ city }: Props) {
-  return <li className="list-group-item">{city}</li>;
+function ListItem({ city, index }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const handleClick = (city: string, index: number) => {
+    alert(`${city} at index ${index} was clicked!`);
+  };
+  return (
+    <li
+      onClick={() => handleClick(city, index)}
+      className="list-group-item"
+      key={city}
+    >
+      {city}
+    </li>
+  );
 }
 
 export default ListItem;
