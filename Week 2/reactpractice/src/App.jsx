5a417d6f68c94a3321a1e78d57035ee134dfa140
jyang-214 <jyang214@my.bcit.ca>
2023-04-25 18:29:30 -0700

    Update App.jsx

--- Week 2/reactpractice/src/App.jsx
+++ Week 2/reactpractice/src/App.jsx
@@ -7,7 +7,19 @@ import Profile from "./Profile";
 // OR multiple lines with parentheses
 
 function App() {
+  const profiles = [
+    { name: "Sandra", url: "https://i.imgur.com/MK3eW3Am.jpg" },
+    { name: "James", url: "https://i.imgur.com/7vQD0fPs.jpg" },
+  ];
 
+  return (
+    <>
+      <h1>My Website</h1>
+      {profiles.map((profile) => (
+        <Profile name={profile.name} url={profile.url} />
+      ))}
+    </>
+  );
 }
 
 function Movies() {}
