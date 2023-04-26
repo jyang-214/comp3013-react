e08fe584a35a045db0b1cd5c1e89353cb87de810
jyang-214 <jyang214@my.bcit.ca>
2023-04-25 20:01:20 -0700

    Week 3

--- Week 3/typescript-practice/src/components/ListGroup.tsx
+++ Week 3/typescript-practice/src/components/ListGroup.tsx
@@ -1,14 +1,54 @@
 import React from "react";
 import ListItem from "./ListItem";
-const cities = ["Vancouver", "London", "Paris", "Tokyo"];
+// const cities: string[] = ["Vancouver", "London", "Paris", "Tokyo"];
+const cities: string[] = [];
 
 function ListGroup() {
+  // //Option 1
+
+  //   // Case 1: Empty Array
+  //   if (cities.length === 0) {
+  //     return <h3>No cities to select!</h3>;
+  //   }
+  //   // Case 2: Ciities in Array..Show Them
+  //   return (
+  //     <ul className="list-group">
+  //       {cities.map((c) => (
+  //         <ListItem city={c} />
+  //       ))}
+  //     </ul>
+  //   );
+
+  // //Option 2
+
+  // return (
+  //   <>
+  //     {/* Case 1: No cities in Array */}
+  //     {cities.length === 0 ? <p>NO CITIES TO SELECT</p> : null}
+
+  //     {/* Case 2: Cities in Array */}
+  //     <ul className="list-group">
+  //       {cities.map((c) => (
+  //         <ListItem city={c} />
+  //       ))}
+  //     </ul>
+  //   </>
+  // );
+
+  // Option 3
+
   return (
-    <ul className="list-group">
-      {cities.map((c) => (
-        <ListItem city={c} />
-      ))}
-    </ul>
+    <>
+      {/* Case 1: No cities in Array */}
+      {cities.length === 0 && <p>NO CITIES TO SELECT</p>}
+
+      {/* Case 2: Cities in Array */}
+      <ul className="list-group">
+        {cities.map((c) => (
+          <ListItem city={c} />
+        ))}
+      </ul>
+    </>
   );
 }
 
