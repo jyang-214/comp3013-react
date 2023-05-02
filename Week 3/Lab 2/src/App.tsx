601fa152f741b20dd912786fa10f6d2a071b66e1
jyang-214 <jyang214@my.bcit.ca>
2023-05-01 22:33:45 -0700

    Lab 2

---
+++ Week 3/Lab 2/src/App.tsx
@@ -0,0 +1,13 @@
+import Viewer from "./components/Viewer";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return (
+    <>
+      <Viewer />
+      <ListGroup />
+    </>
+  );
+}
+
+export default App;
